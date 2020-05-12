ee83dd80aa8a82e2be2b7e3666780d70886d68c7
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2020-05-13 02:22:03 +0300

    Fixed UART baud rate error
    Baud rate set error caused incorrect data reception of data packages

--- Sources/main.c
+++ Sources/main.c
@@ -12,18 +12,24 @@
 
 int main(void)
 {
-	uart0_init(UART_BAUD_SELECT(115200, F_CPU));
-	uart1_init(UART_BAUD_SELECT(115200, F_CPU));
+	uart0_init(UART_BAUD_SELECT_DOUBLE_SPEED(115200, F_CPU));
+	uart1_init(UART_BAUD_SELECT_DOUBLE_SPEED(115200, F_CPU));
+
     stepperSetup();
 
 	while(1) {
 		_delay_ms(10);
-		uint16_t c = uart1_getc();
+		uint16_t c0 = uart0_getc();
 
-		if (c == UART_NO_DATA) {
-			continue;
+		if (c0 != UART_NO_DATA) {
+    		uart0_putc(c0 & 0xff);
+    		uart1_putc(c0 & 0xff);
 		}
 
-		uart0_putc(c & 0xff);
+        uint16_t c1 = uart1_getc();
+        if (c1 != UART_NO_DATA) {
+            uart0_putc(c1 & 0xff);
+            uart1_putc(c1 & 0xff);
+        }
 	}
 }
