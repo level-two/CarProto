679120167f731a33e5d126a03527145ee6dc5a0b
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2020-05-27 18:55:51 +0300

    Fixes:
    - Command porcessing issue
    - Print busy status

--- Sources/main.c
+++ Sources/main.c
@@ -17,12 +17,16 @@ static SteerStatePtr steerInit();
 
 int main(void)
 {
-	uart1_init(UART_BAUD_SELECT_DOUBLE_SPEED(115200, F_CPU));
+    uart0_init(UART_BAUD_SELECT_DOUBLE_SPEED(115200, F_CPU));
+    uart1_init(UART_BAUD_SELECT_DOUBLE_SPEED(115200, F_CPU));
 
     SteerStatePtr steer = steerInit();
     if (steer == NULL) return -1;
 
     timer0Start();
+
+    uart0_puts("Hi Jack!");
+    uart1_puts("Hi Jack!");
 
 	while(1) {
         uint16_t dt = timer0Microseconds();
@@ -30,23 +34,44 @@ int main(void)
 
         steerUpdate(steer, dt);
 
-        if (uart1_available()) {
-            uint8_t command = (uint8_t)uart1_getc();
+        uint8_t command = 0;
 
-            switch (command) {
-            case 'L':
-                steerSetPosition(steer, steerPositionLeft);
-                uart1_puts("Turning left");
-                break;
-            case 'R':
-                steerSetPosition(steer, steerPositionLeft);
-                uart1_puts("Turning right");
-                break;
-            case 'M':
-                steerSetPosition(steer, steerPositionLeft);
-                uart1_puts("Turning middle");
-                break;
+        if (uart0_available()) {
+            command = (uint8_t)uart0_getc();
+
+        } else if (uart1_available()) {
+            command = (uint8_t)uart1_getc();
+            if (steerIsBusy(steer)) {
+                uart0_puts("Busy");
+                uart1_puts("Busy");
+                continue;
             }
+        }
+
+        if (command != 0 && steerIsBusy(steer)) {
+            uart0_puts("Busy");
+            uart1_puts("Busy");
+            command = 0;
+        }
+
+        switch (command) {
+        case 'L':
+            steerSetPosition(steer, steerPositionLeft);
+            uart0_puts("Turning left");
+            uart1_puts("Turning left");
+            break;
+        case 'R':
+            steerSetPosition(steer, steerPositionRight);
+            uart0_puts("Turning right");
+            uart1_puts("Turning right");
+            break;
+        case 'M':
+            steerSetPosition(steer, steerPositionMiddle);
+            uart0_puts("Turning middle");
+            uart1_puts("Turning middle");
+            break;
+        default:
+            break;
         }
 
 		_delay_us(100);
